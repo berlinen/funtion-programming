89bdf90f69767298267520e86350a52d2842bcbf
weibin <1292393108@qq.com>
2019-06-17 11:01:48 +0800

    type inference

--- typeScript/typeInference/demo-1.ts
+++ typeScript/typeInference/demo-1.ts
@@ -1,1 +1,19 @@
 // 上下文打字
+// 在TypeScript中，类型推断在某些情况下也适用于“另一个方向”。这被称为“上下文打字”。
+window.onmousedown = function(uiElement) {
+  console.log(uiElement.button) // => ok
+  console.log(uiElement.kangaroo) // => Error
+}
+
+// --------------------
+
+window.onscroll = function(e) {
+  console.log(e.button) // =>error
+}
+
+// any
+
+window.onscroll = function(uiEvent: any) {
+  console.log(uiEvent.button);  //<- Now, no error is given
+};
+
