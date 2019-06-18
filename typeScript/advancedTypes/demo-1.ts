d3556ffd713b80b9b3d0254037b6104cb66caad6
weibin <1292393108@qq.com>
2019-06-18 14:09:25 +0800

    union

---
+++ typeScript/advancedTypes/demo-1.ts
@@ -0,0 +1,33 @@
+// intersection types
+// 交叉类型
+function extend<First, Second>(first: First, second: Second): First & Second {
+  const result: Partial<First & Second> = {}
+  for (const prop in first) {
+    if (first.hasOwnProperty(prop)) {
+        (<First>result)[prop] = first[prop];
+        }
+    }
+  for (const prop in second) {
+      if (second.hasOwnProperty(prop)) {
+          (<Second>result)[prop] = second[prop];
+      }
+  }
+  return <First & Second>result;
+}
+
+class Person {
+  constructor(public name: string) { }
+}
+
+interface Loggable {
+  log(name: string): void;
+}
+
+class ConsoleLogger implements Loggable {
+  log(name) {
+      console.log(`Hello, I'm ${name}.`);
+  }
+}
+
+const jim = extend(new Person('Jim'), ConsoleLogger.prototype);
+jim.log(jim.name);
