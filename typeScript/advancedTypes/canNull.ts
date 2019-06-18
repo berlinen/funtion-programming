521e28190dacbecad579e4209d2bb63b12114b44
weibin <1292393108@qq.com>
2019-06-18 16:37:45 +0800

    type

--- typeScript/advancedTypes/canNull.ts
+++ typeScript/advancedTypes/canNull.ts
@@ -6,3 +6,17 @@ f(1, 2);
 f(1);
 f(1, undefined);
 f(1, null); // error, 'null' is not assignable to 'number | undefined'
+
+
+// ````````````````````````````````````````````````````````````````````````````
+
+class C {
+  a: number;
+  b?: number;
+}
+let c = new C();
+c.a = 12;
+c.a = undefined; // error, 'undefined' is not assignable to 'number'
+c.b = 13;
+c.b = undefined; // ok
+c.b = null; // error, 'null' is not assignable to 'number | undefined'
---
+++ typeScript/advancedTypes/typeAssertions.ts
@@ -0,0 +1,14 @@
+// 由于可以使用union实现可空类型，因此需要使用类型保护来删除null。幸运的是，这与您在JavaScript中编写的代码相同：
+function f(sn: string | null): string {
+  if (sn == null) {
+      return "default";
+  }
+  else {
+      return sn;
+  }
+}
+// ull消除是很明显的在这里，但你可以用更简洁运营商太：
+function f2(sn: string | null): string {
+  return sn || 'default'
+}
+
